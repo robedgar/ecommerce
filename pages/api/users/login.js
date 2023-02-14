d0656fc78aeb152256703d79e36201c6cd4077cc
robedgar <robertiedgar@me.com>
2023-02-14 20:42:52 +0000

    add auth

---
+++ pages/api/users/login.js
@@ -0,0 +1,39 @@
+import { createRouter } from 'next-connect';
+import db from '../../utils/db';
+import User from '../../models/User';
+import bcrypt from 'bcryptjs';
+import { signToken } from '../../../utils/auth';
+
+// Default Req and Res are IncomingMessage and ServerResponse
+// You may want to pass in NextApiRequest and NextApiResponse
+const router = createRouter();
+
+router.get(async (req, res) => {
+  await db.connect();
+  const user = await User.findOne({ email: req.body.email });
+  await db.disconnect();
+  if (user && bcrypt.compareSync(req.body.password, user.password)) {
+    const token = signToken(user);
+    res.send({
+      token,
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(401).send({ message: 'Invalid email or password' });
+  }
+});
+
+// create a handler from router with custom
+// onError and onNoMatch
+export default router.handler({
+  onError: (err, req, res) => {
+    console.error(err.stack);
+    res.status(500).end('Something broke!');
+  },
+  onNoMatch: (req, res) => {
+    res.status(404).end('Page is not found');
+  },
+});
